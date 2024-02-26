01d650aa2a0e82bdf34c3b2f8b2662ea6e33247f
Andrey Belomyttsev <andymedtech@gmail.com>
2024-02-26 12:58:14 +0300

    routing with / and public

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+// app.use(express.static("cats"));
+
+app.get("/", function (req, res) {
+  console.log("Request success");
+  res.end("Request success");
+});
+
+app.get("/cats", function (req, res) {
+  res.end("The cats are very fluffy");
+});
+
+app.get("/dogs", function (req, res) {
+  res.end("Dogs are very smart");
+});
+
+app.get("/mouses", function (req, res) {
+  res.end("The mouses are small");
+});
+
+app.get("/horses", function (req, res) {
+  res.end("Horses are fast");
+});
+
+app.get("/birds", function (req, res) {
+  res.end("Birds can fly");
+});
+
+app.listen(8080, function () {
+  console.log("Server has been started on port 8080");
+});
+
+// http://localhost:8080/cats
+// http://localhost:8080/dogs
+// http://localhost:8080/mouses
+// http://localhost:8080/horses
+// http://localhost:8080/birds
